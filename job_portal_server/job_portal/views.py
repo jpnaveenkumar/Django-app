ca2e1a714a867f0016fee1eb5d415385c3202438
jpnaveenkumar1998@gmail.com <jpnaveenkumar1998@gmail.com>
2021-01-10 19:10:49 +0800

    views.py refactored

--- job_portal_server/job_portal/views.py
+++ job_portal_server/job_portal/views.py
@@ -10,39 +10,61 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser 
 
 # Create your views here.
-# def home(request):
-#     return HttpResponse("This is Naveen's Homepage")
+
+def handleSuccessResponse(data, status):
+    response = {
+        "status" : "success",
+        "data" : data
+    }
+    return JsonResponse(response, status = status, safe = False)
+
+def handleFailureResponse(message, status):
+    response = {
+        "status" : "error",
+        "data" : message
+    }
+    return JsonResponse(response, status = status, safe = False)
+
+def getAllJobs():
+    jobs = Jobs.objects.all()
+    job_serializer = JobSerializer(jobs, many=True)
+    return handleSuccessResponse(job_serializer.data, status.HTTP_200_OK)
+
+def createJob(jobRequest):
+    jobToInsert = JSONParser().parse(jobRequest)
+    job_serializer = JobSerializer(data = jobToInsert)
+    if job_serializer.is_valid():
+        job_serializer.save()
+        return handleSuccessResponse(job_serializer.data, status.HTTP_201_CREATED)
+    return handleFailureResponse(job_serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+def updateJob(jobRequest):
+    jobToUpdate = JSONParser().parse(jobRequest)
+    job_serializer = JobSerializer(job, data = jobToUpdate)
+    if job_serializer.is_valid():
+        job_serializer.save()
+        return handleSuccessResponse(job_serializer.data, status.HTTP_201_CREATED)
+    else:
+        return handleFailureResponse(tutorial_serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
 def job(request):
     if request.method == 'GET':
-        jobs = Jobs.objects.all()
-        job_serializer = JobSerializer(jobs, many=True)
-        return JsonResponse(job_serializer.data, safe= False)
+        return getAllJobs()
     if request.method == 'POST':
-        jobToInsert = JSONParser().parse(request)
-        job_serializer = JobSerializer(data = jobToInsert)
-        if job_serializer.is_valid():
-            job_serializer.save()
-            return JsonResponse(job_serializer.data, status=status.HTTP_201_CREATED)
+        return createJob(request)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def specificJob(request, job_id):
     try:
         job = Jobs.objects.get(id = job_id)
     except Jobs.DoesNotExist:
-        return JsonResponse({'message': 'The Job with given Id does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        return handleFailureResponse('The Job with given Id does not exist', status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         job_serializer = JobSerializer(job)
-        return JsonResponse(job_serializer.data, safe= False)
+        return handleSuccessResponse(job_serializer.data, status.HTTP_200_OK)
     if request.method == 'PUT':
-        jobToUpdate = JSONParser().parse(request)
-        job_serializer = JobSerializer(job, data = jobToUpdate)
-        if job_serializer.is_valid():
-            job_serializer.save()
-            return JsonResponse(job_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return updateJob(request)
     if request.method == 'DELETE':
         job.delete()
-        return JsonResponse({'message': 'Job was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+        return handleSuccessResponse('Job was deleted successfully!', status.HTTP_204_NO_CONTENT)
